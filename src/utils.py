4bffed25a5818a005beb53428473ff41ab9cc58d
princeX02 <princechaudhary4366@gamil.com>
2025-05-15 21:52:14 +0530

    Predication Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import dill 
+import dill
 
 import numpy as np
 import pandas as pd
@@ -9,33 +9,37 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
         
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
-    
+        raise CustomException(e, sys)
 
-def evaluate_models(x_train,y_train,x_test,y_test, models,param):
+def load_object(file_path):
     try:
-       
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
+    try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+            para = param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(x_train, y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(x_train,y_train)
-            #model.fit(x_train, y_train)  # Train model
+            model.fit(x_train, y_train)
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -47,5 +51,4 @@ def evaluate_models(x_train,y_train,x_test,y_test, models,param):
         
         return report
     except Exception as e:
-        raise CustomException(e,sys)
-    
+        raise CustomException(e, sys)
