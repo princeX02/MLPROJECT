ab00fb28e8bd8d120690eecd776aee2e5ed7072d
princeX02 <princechaudhary4366@gamil.com>
2025-05-15 20:59:24 +0530

    Hyperparameter train

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,12 +43,42 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
             }
 
-            model_report = evaluate_models(x_train, y_train, x_test, y_test, models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBoost": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoost": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "K-Neighbors": {
+                    'n_neighbors': [3, 5, 7],
+                    'weights': ["uniform", "distance"]
+                }
+            }
+
+            model_report: dict = evaluate_models(x_train, y_train, x_test, y_test, models=models, param=params)
 
             best_model_score = max(model_report.values())
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
